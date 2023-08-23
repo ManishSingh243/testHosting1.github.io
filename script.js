5b87398cd87fa8e839c85de3bc11e057419b75ea
ManishSingh243 <87477768+ManishSingh243@users.noreply.github.com>
2023-08-24 01:52:07 +0530

    Update script.js

--- script.js
+++ script.js
@@ -2,6 +2,6 @@ document.addEventListener("DOMContentLoaded", function() {
     const button = document.getElementById("clickButton");
     
     button.addEventListener("click", function() {
-        alert("Congratulations! Chandrayaan 3 has been landed successfully on Moon. Proud to be an Indian🇮🇳");
+        alert("Congratulations! Chandrayaan 3 has been landed successfully on Moon. Proud to be an Indian.");
     });
 });
